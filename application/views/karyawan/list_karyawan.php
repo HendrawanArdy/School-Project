4a026ee933c86bc4f0c18015fdddf65a754ffe76
HendrawanArdy <ardyprihantana26@gmail.com>
2019-01-14 14:37:45 +0700

    initial commit

---
+++ application/views/karyawan/list_karyawan.php
@@ -0,0 +1,9 @@
+<?php 
+foreach($karyawan as $karyawan) :
+ echo $karyawan->karyawan_id;
+ echo $karyawan->nama;
+ echo $karyawan->alamat;
+ echo $karyawan->telepon;
+ echo "</br>";
+ endforeach; 
+ ?>
